9d1a64a8c9f92cf0edc38941f4df42779ef61cb0
Oleksandr Bondarevskyi <epickonst@gmail.com>
2026-05-04 16:24:19 +0300

    Тесты для каталога

--- src/main/ui/tests/test_catalog.py
+++ src/main/ui/tests/test_catalog.py
@@ -1,7 +1,7 @@
 from playwright.sync_api import expect
 
 
-def test_count_items(page):
+def test_count_catalog(page):
     page.goto("https://saucedemo.com/")
 
     page.get_by_placeholder("Username").fill("standard_user")
@@ -30,7 +30,7 @@ def test_sorted_by_name(page):
     assert names == sorted(names), "Товары не отсортированы по имени A-Z"
 
 
-def test_sort_by_name_reversed(page):
+def test_sort_by_name_z_to_a(page):
     page.goto("https://saucedemo.com/")
 
     page.get_by_placeholder("Username").fill("standard_user")
@@ -45,6 +45,7 @@ def test_sort_by_name_reversed(page):
     names = page.locator(".inventory_item_name").all_text_contents()
 
     assert names == sorted(names, reverse=True), "Товары не отсортированы по имени Z-A"
+
 
 def test_sort_by_price(page):
     page.goto("https://saucedemo.com/")
@@ -69,7 +70,6 @@ def test_sort_by_price(page):
 
     prices = [float(p.replace("$", "")) for p in prices_text]
 
-
     assert prices == sorted(prices, reverse=True), "Цены не отсортированы от большей к меньшей"
 
 
@@ -89,3 +89,99 @@ def test_add_to_cart(page):
     expect(page).to_have_url("https://www.saucedemo.com/cart.html")
 
     expect(page.locator(".inventory_item_name")).to_have_text("Sauce Labs Bike Light")
+
+
+def test_add_to_cart_sauce_labs_onesie(page):
+    page.goto("https://saucedemo.com/")
+
+    page.get_by_placeholder("Username").fill("standard_user")
+    page.get_by_placeholder("Password").fill("secret_sauce")
+    page.locator("#login-button").click()
+
+    product_card = page.locator(".inventory_item", has_text="Sauce Labs Onesie")
+    add_button = product_card.locator("button")
+
+    add_button.click()
+    expect(add_button).to_have_text("Remove")
+
+    expect(page.locator(".shopping_cart_badge")).to_contain_text("1")
+
+    add_button.click()
+    expect(add_button).to_have_text("Add to cart")
+    expect(page.locator(".shopping_cart_badge")).to_be_hidden()
+
+
+def test_product_details_onesie(page):
+    page.goto("https://saucedemo.com/")
+    page.get_by_placeholder("Username").fill("standard_user")
+    page.get_by_placeholder("Password").fill("secret_sauce")
+    page.locator("#login-button").click()
+
+    product_card = page.locator(".inventory_item", has_text="Sauce Labs Onesie")
+
+    product_name = product_card.locator('[data-test="inventory-item-name"]').inner_text()
+    product_price = product_card.locator('[data-test="inventory-item-price"]').inner_text()
+
+    product_card.locator('[data-test="inventory-item-name"]').click()
+
+    detail_name = page.locator('[data-test="inventory-item-name"]').inner_text()
+    detail_price = page.locator('[data-test="inventory-item-price"]').inner_text()
+
+    assert product_name == detail_name, "Имя товара не совпадает"
+    assert product_price == detail_price, "Цена товара не совпадает"
+
+
+def test_product_details_fleece_jacket(page):
+    page.goto("https://saucedemo.com/")
+    page.get_by_placeholder("Username").fill("standard_user")
+    page.get_by_placeholder("Password").fill("secret_sauce")
+    page.locator("#login-button").click()
+
+    product_card = page.locator(".inventory_item", has_text="Sauce Labs Fleece Jacket")
+
+    product_name = product_card.locator('[data-test="inventory-item-name"]').inner_text()
+    product_price = product_card.locator('[data-test="inventory-item-price"]').inner_text()
+
+    product_card.locator('[data-test="inventory-item-name"]').click()
+
+    detail_name = page.locator('[data-test="inventory-item-name"]').inner_text()
+    detail_price = page.locator('[data-test="inventory-item-price"]').inner_text()
+
+    assert product_name == detail_name, "Имя товара не совпадает"
+    assert product_price == detail_price, "Цена товара не совпадает"
+
+
+def test_remove_item_from_catalog(page):
+    page.goto("https://saucedemo.com/")
+    page.get_by_placeholder("Username").fill("standard_user")
+    page.get_by_placeholder("Password").fill("secret_sauce")
+    page.locator("#login-button").click()
+
+    product_card = page.locator(".inventory_item", has_text="Test.allTheThings() T-Shirt (Red)")
+    product_button = product_card.locator('[data-test="add-to-cart-test.allthethings()-t-shirt-(red)"]')
+    product_button.click()
+
+    remove_button = product_card.locator('[data-test="remove-test.allthethings()-t-shirt-(red)"]')
+    assert remove_button.is_visible(), "Кнопка Remove не появилась"
+
+    remove_button.click()
+
+    assert product_button.is_visible(), "Кнопка Add to cart не вернулась после удаления товара"
+
+
+def test_remove_onesie_from_catalog(page):
+    page.goto("https://saucedemo.com/")
+    page.get_by_placeholder("Username").fill("standard_user")
+    page.get_by_placeholder("Password").fill("secret_sauce")
+    page.locator("#login-button").click()
+
+    product_card = page.locator(".inventory_item", has_text="Sauce Labs Onesie")
+    product_button = page.locator('[data-test="add-to-cart-sauce-labs-onesie"]')
+    product_button.click()
+
+    remove_button = product_card.locator('[data-test="remove-sauce-labs-onesie"]')
+    assert remove_button.is_visible()
+
+    remove_button.click()
+
+    assert product_button.is_visible()
